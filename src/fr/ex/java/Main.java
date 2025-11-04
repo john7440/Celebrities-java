536a4dbc6ebb7fec464f45c6821b55a8777ae381
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-04 10:22:33 +0100

    ajout d'une hashmap avec la liste des invités

--- src/fr/ex/java/Main.java
+++ src/fr/ex/java/Main.java
@@ -1,10 +1,26 @@
 package fr.ex.java;
+import java.util.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Map<String, List<Integer>> guestList = new HashMap<>();
 
+		
+		// Liste invités
+		guestList.put("Albert", Arrays.asList(2, 5, 6));
+		guestList.put("Bénédicte", Arrays.asList(3, 5, 6));
+		guestList.put("Christophe", Arrays.asList(2, 4, 5, 6));
+		guestList.put("Delphine", Arrays.asList(1, 2, 5, 6, 8));
+		guestList.put("Edouard", Arrays.asList(6));
+		guestList.put("Françoise", Arrays.asList(5));
+		guestList.put("Gaston", Arrays.asList(2, 3, 5, 6, 8));
+		guestList.put("Heloïse", Arrays.asList(2, 4, 5, 6, 7));
+		
+		
 	}
 
 }
