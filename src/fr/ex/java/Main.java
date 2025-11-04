03748d8bbf56b743a55bb3599e0bc218aa004519
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-04 10:46:10 +0100

    modification de la structure de donnés pour lier le numéro d'invité a son nom et à ses connaissances

--- src/fr/ex/java/Main.java
+++ src/fr/ex/java/Main.java
@@ -6,19 +6,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
-		Map<String, List<Integer>> guestList = new HashMap<>();
+		// le nom des invité lié a un numéro d'invité
+		Map<Integer, String> guestNames = Map.of(
+				1, "Albert",
+				2, "Bénédicte",
+				3, "Christophe",
+				4, "Delphine",
+				5, "Edouard",
+				6, "Françoise",
+				7, "Gaston",
+				8, "Héloïse"
+			);
 
-		
-		// Liste invités
-		guestList.put("Albert", Arrays.asList(2, 5, 6));
-		guestList.put("Bénédicte", Arrays.asList(3, 5, 6));
-		guestList.put("Christophe", Arrays.asList(2, 4, 5, 6));
-		guestList.put("Delphine", Arrays.asList(1, 2, 5, 6, 8));
-		guestList.put("Edouard", Arrays.asList(6));
-		guestList.put("Françoise", Arrays.asList(5));
-		guestList.put("Gaston", Arrays.asList(2, 3, 5, 6, 8));
-		guestList.put("Heloïse", Arrays.asList(2, 4, 5, 6, 7));
+		// les relations entre les invités
+		Map<Integer, List<Integer>> knownGuest = Map.of(
+				1, Arrays.asList(2, 5, 6),
+				2, Arrays.asList(3, 5, 6),
+				3, Arrays.asList(2, 4, 5, 6),
+				4, Arrays.asList(1, 2, 5, 6, 8),
+				5, Arrays.asList(6),
+				6, Arrays.asList(5),
+				7, Arrays.asList(2, 3, 5, 6, 8),
+				8, Arrays.asList(2, 4, 5, 6, 7)
+			);
 		
 		
 	}
