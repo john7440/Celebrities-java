75c9b598c04daef4587b5a8a2c267f44560f4912
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-04 10:54:52 +0100

    amélioration du systeme de structure des invités

--- src/fr/ex/java/Main.java
+++ src/fr/ex/java/Main.java
@@ -6,31 +6,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// le nom des invité lié a un numéro d'invité
-		Map<Integer, String> guestNames = Map.of(
-				1, "Albert",
-				2, "Bénédicte",
-				3, "Christophe",
-				4, "Delphine",
-				5, "Edouard",
-				6, "Françoise",
-				7, "Gaston",
-				8, "Héloïse"
-			);
+		// noms des invités
+        Map<Integer, String> guestNames = new HashMap<>();
+        guestNames.put(1, "Albert");
+        guestNames.put(2, "Bénédicte");
+        guestNames.put(3, "Christophe");
+        guestNames.put(4, "Delphine");
+        guestNames.put(5, "Edouard");
+        guestNames.put(6, "Françoise");
+        guestNames.put(7, "Gaston");
+        guestNames.put(8, "Héloïse");
 
-		// les relations entre les invités
-		Map<Integer, List<Integer>> knownGuest = Map.of(
-				1, Arrays.asList(2, 5, 6),
-				2, Arrays.asList(3, 5, 6),
-				3, Arrays.asList(2, 4, 5, 6),
-				4, Arrays.asList(1, 2, 5, 6, 8),
-				5, Arrays.asList(6),
-				6, Arrays.asList(5),
-				7, Arrays.asList(2, 3, 5, 6, 8),
-				8, Arrays.asList(2, 4, 5, 6, 7)
-			);
-		
-		
+        // connaissances des autres invités
+        Map<Integer, List<Integer>> knownGuests = new HashMap<>();
+        knownGuests.put(1, Arrays.asList(2, 5, 6));
+        knownGuests.put(2, Arrays.asList(3, 5, 6));
+        knownGuests.put(3, Arrays.asList(2, 4, 5, 6));
+        knownGuests.put(4, Arrays.asList(1, 2, 5, 6, 8));
+        knownGuests.put(5, Arrays.asList(6));
+        knownGuests.put(6, Arrays.asList(5));
+        knownGuests.put(7, Arrays.asList(2, 3, 5, 6, 8));
+        knownGuests.put(5, Arrays.asList(2, 4, 5, 6 , 7));
 	}
 
 }
